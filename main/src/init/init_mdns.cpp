d9c474d0c3072f1816d34a7257061c26dd6db75b
syoch <syoch64@gmail.com>
2023-06-05 03:52:38 +0900

    Update: use mdns_hostname config variable

--- main/src/init/init_mdns.cpp
+++ main/src/init/init_mdns.cpp
@@ -16,10 +16,10 @@ void init::init_mdns() {
   ESP_ERROR_CHECK(mdns_init());
 
   ESP_LOGI(TAG, "- 1/3 Set hostname...");
-  mdns_hostname_set("esp32");
+  mdns_hostname_set(CONFIG_MDNS_HOSTNAME);
   mdns_instance_name_set("ESP32");
   ESP_LOGI(TAG, "- 2/3 Add service...");
-  mdns_service_add(NULL, "_tcp", "_tcp", 2000, NULL, 0);
+  mdns_service_add(NULL, "_tcp", "_tcp", 4007, 2, 0);
   ESP_LOGI(TAG, "- 3/3 mDNS is setted up!...");
 #else
   ESP_LOGW(TAG, "mDNS is disabled.");
