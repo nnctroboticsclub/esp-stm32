463a784cad3dd1947d81111e6ed9e70c6a612410
syoch <syoch64@gmail.com>
2023-07-11 21:34:50 +0900

    FIX SOME SPI BL ERROR

--- components/stm32_bootloader/src/stm32bl/stm32bl_spi.cpp
+++ components/stm32_bootloader/src/stm32bl/stm32bl_spi.cpp
@@ -1,24 +1,25 @@
 #include <stm32bl/stm32bl_spi.hpp>
 
 #include <memory>
+#include <ranges>
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
 #include <esp_log.h>
+#include <esp_debug_helpers.h>
 
 namespace connection::application::stm32bl {
 void Stm32BootLoaderSPI::WaitACKFrame() {
-  ESP_LOGI(TAG, "Wait ACK Frame");
+  this->device.SetTraceEnabled(false);
   int fail_count = 0;
-  std::vector<uint8_t> buf;
 
-  buf = {0x5A};
-  auto ret = this->device->transfer(buf).get();
+  this->device.SendChar(0x5A);
+  this->device.RecvChar();
 
   while (true) {
-    buf = {0x00};
-    if (auto ch = this->device->transfer(buf).get()[0]; ch == 0x79) {
+    this->device.SendChar(0x00);
+    if (auto ch = this->device.RecvChar(); ch == 0x79) {
       break;
     } else if (ch == 0x1f) {
       ESP_LOGW(TAG, "NACK");
@@ -35,8 +36,9 @@ void Stm32BootLoaderSPI::WaitACKFrame() {
       }
     }
   }
-  buf = {0x79};
-  ret = this->device->transfer(buf).get();
+  this->device.SendChar(0x79);
+  this->device.RecvChar();
+  this->device.SetTraceEnabled(true);
   return;
 }
 
@@ -45,11 +47,10 @@ void Stm32BootLoaderSPI::Synchronization() {
 
   std::vector<uint8_t> buf;
 
-  buf = {0x5A};
+  this->device.SendChar(0x5A);
 
-  if (auto ret = this->device->transfer(buf).get(); ret[0] != 0xA5) {
-    ESP_LOGE(TAG, "Failed Sync (ACK Return value = %#02x != %#02x)", ret[0],
-             0xA5);
+  if (auto ret = this->device.RecvChar(); ret != 0xA5) {
+    ESP_LOGE(TAG, "Failed Sync (ACK Return value = %#02x != %#02x)", ret, 0xA5);
     throw ACKFailed();
   }
   this->WaitACKFrame();
@@ -61,8 +62,9 @@ void Stm32BootLoaderSPI::Synchronization() {
 
 void Stm32BootLoaderSPI::CommandHeader(uint8_t cmd) {
   std::vector<uint8_t> buf{0x5A, cmd, (uint8_t)(cmd ^ 0xff)};
+  this->device.Send(buf);
 
-  this->device->transfer(buf);
+  this->device.Recv(buf);
 
   this->WaitACKFrame();
 
@@ -70,20 +72,22 @@ void Stm32BootLoaderSPI::CommandHeader(uint8_t cmd) {
 }
 
 void Stm32BootLoaderSPI::ReadData(std::vector<uint8_t> &buf) {
-  std::vector<uint8_t> dummy = {0x00};
-  this->device->transfer(dummy).get();
+  this->device.SendChar(0x00);
+  this->device.RecvChar();
 
   this->ReadDataWithoutHeader(buf);
 }
 
 void Stm32BootLoaderSPI::ReadDataWithoutHeader(std::vector<uint8_t> &buf) {
-  this->device->transfer(buf).get();
+  std::ranges::fill(buf, 0x00);
+
+  this->device.Send(buf);
+  this->device.Recv(buf);
 }
 
 Stm32BootLoaderSPI::Stm32BootLoaderSPI(idf::GPIONum reset, idf::GPIONum boot0,
-                                       idf::SPINum spi_host, idf::CS cs)
-    : STM32BootLoader(reset, boot0),
-      device(std::make_shared<idf::SPIDevice>(spi_host, cs)) {}
+                                       idf::SPIMaster &spi_host, idf::CS cs)
+    : STM32BootLoader(reset, boot0), device(spi_host, cs) {}
 
 Stm32BootLoaderSPI::~Stm32BootLoaderSPI() = default;
 
@@ -110,29 +114,30 @@ void Stm32BootLoaderSPI::Get() {
 
   std::vector<uint8_t> buf(2);
   this->ReadData(buf);
-  this->WaitACKFrame();
   auto n = buf[0];
   auto version = buf[1];
 
   ESP_LOGI(TAG, "Bootloader version: %d.%d", version >> 4, version & 0x0f);
 
-  std::vector<uint8_t> commands(n);
-  this->ReadDataWithoutHeader(commands);
+  std::vector<uint8_t> raw_command(n);
+  this->ReadDataWithoutHeader(raw_command);
 
-  this->commands.get = commands[0];
-  this->commands.get_version = commands[1];
-  this->commands.get_id = commands[2];
-  this->commands.read_memory = commands[3];
-  this->commands.go = commands[4];
-  this->commands.write_memory = commands[5];
-  this->commands.erase = commands[6];
-  this->commands.write_protect = commands[7];
-  this->commands.write_unprotect = commands[8];
-  this->commands.readout_protect = commands[9];
-  this->commands.readout_unprotect = commands[10];
+  this->commands.get = raw_command[0];
+  this->commands.get_version = raw_command[1];
+  this->commands.get_id = raw_command[2];
+  this->commands.read_memory = raw_command[3];
+  this->commands.go = raw_command[4];
+  this->commands.write_memory = raw_command[5];
+  this->commands.erase = raw_command[6];
+  this->commands.write_protect = raw_command[7];
+  this->commands.write_unprotect = raw_command[8];
+  this->commands.readout_protect = raw_command[9];
+  this->commands.readout_unprotect = raw_command[10];
   if (n > 0x0c) {
-    this->commands.get_checksum = buf[13];
+    this->commands.get_checksum = raw_command[11];
   }
+
+  this->WaitACKFrame();
   return;
 }
 
@@ -140,12 +145,13 @@ void Stm32BootLoaderSPI::Erase(SpecialFlashPage page) {
   ESP_LOGI(TAG, "Erasing %s", SpecialFlashPageToString(page).c_str());
   this->CommandHeader(this->commands.erase);
 
-  std::vector<uint8_t> buf;
+  std::vector<uint8_t> buf(3);
   buf[0] = ((uint16_t)page >> 8);
   buf[1] = ((uint16_t)page & 0xff);
   buf[2] = buf[0] ^ buf[1];
 
-  this->device->transfer(buf).get();
+  this->device.Send(buf);
+  this->device.Recv(buf);
   this->WaitACKFrame();
 
   return;
@@ -157,27 +163,33 @@ void Stm32BootLoaderSPI::Erase(std::vector<FlashPage> &pages) {
            0x0800'0000 + pages[pages.size() - 1] * 0x800);
   this->CommandHeader(this->commands.erase);
 
+  uint8_t checksum = 0;
+
   // Send a pages
   {
-    std::vector<uint8_t> buf{};
+    std::vector<uint8_t> buf(3);
     buf[0] = pages.size() >> 8;
     buf[1] = pages.size() & 0xff;
-    buf[2] = CalculateChecksum(buf);
-    this->device->transfer(buf).get();
+    checksum ^= CalculateChecksum(buf);
+    buf[2] = checksum;
+
+    this->device.Send(buf);
+    this->device.Recv(buf);
     this->WaitACKFrame();
   }
 
   // Send pages and checksum
   {
-    // auto buf = new uint8_t[pages.size() * 2];
-    std::vector<uint8_t> buf(pages.size() * 2, 0);
+    std::vector<uint8_t> buf(pages.size() * 2, 0x77);
     for (size_t i = 0; i < pages.size(); i++) {
       buf[2 * i] = pages[i] >> 8;
       buf[2 * i + 1] = pages[i] & 0xff;
     }
-    buf.push_back(CalculateChecksum(buf));
+    checksum = 0x5a ^ CalculateChecksum(buf);  // WHAT 0x5A
+    buf.push_back(checksum);
 
-    this->device->transfer(buf).get();
+    this->device.Send(buf);
+    this->device.Recv(buf);
     this->WaitACKFrame();
   }
 
@@ -190,10 +202,17 @@ void Stm32BootLoaderSPI::WriteMemoryBlock(uint32_t addr,
   this->CommandHeader(this->commands.write_memory);
 
   {
-    std::vector<uint8_t> buf{(uint8_t)(addr >> 24), (uint8_t)(addr >> 16),
-                             (uint8_t)(addr >> 8), (uint8_t)(addr & 0xff)};
-    buf[4] = CalculateChecksum(buf);
-    this->device->transfer(buf).get();
+    this->device.SendU32(addr);
+    this->device.RecvU32();
+
+    this->device.SendChar(0x00);
+    this->device.RecvChar();
+
+    // std::vector<uint8_t> buf{(uint8_t)(addr >> 24), (uint8_t)(addr >> 16),
+    //                          (uint8_t)(addr >> 8), (uint8_t)(addr & 0xff)};
+    // buf[4] = CalculateChecksum(buf);
+    // this->device.Send(buf);
+    // this->device.Recv(buf);
 
     this->WaitACKFrame();
   }
@@ -201,11 +220,14 @@ void Stm32BootLoaderSPI::WriteMemoryBlock(uint32_t addr,
   {
     std::vector<uint8_t> buf{(uint8_t)(buffer.size() - 1)};
 
-    this->device->transfer(buf).get();
-    this->device->transfer(buffer).get();
+    this->device.Send(buf);
+    this->device.Recv(buf);
+    this->device.Send(buffer);
+    this->device.Recv(buffer);
 
     buf[0] = CalculateChecksum(buffer) ^ buf[0];
-    this->device->transfer(buf).get();
+    this->device.Send(buf);
+    this->device.Recv(buf);
 
     this->WaitACKFrame();
   }
@@ -220,7 +242,8 @@ void Stm32BootLoaderSPI::Go(uint32_t addr) {
   std::vector<uint8_t> buf{(uint8_t)(addr >> 24), (uint8_t)(addr >> 16),
                            (uint8_t)(addr >> 8), (uint8_t)(addr & 0xff)};
   buf[4] = CalculateChecksum(buf);
-  this->device->transfer(buf).get();
+  this->device.Send(buf);
+  this->device.Recv(buf);
 
   this->WaitACKFrame();
 
