bc0e941cec46099e2b39fce8097d58e48f18299c
syoch <syoch64@gmail.com>
2023-08-28 17:48:24 +0900

    Update: fix variable names

--- main/src/init/init_wifi.cpp
+++ main/src/init/init_wifi.cpp
@@ -1,9 +1,8 @@
-#include "init.hpp"
-
-#include "../config/config.hpp"
-
 #include <esp_log.h>
 #include <nvs_flash.h>
+
+#include "init.hpp"
+#include "../config/config.hpp"
 
 void init::init_wifi() {
   static bool initialized = false;
@@ -12,10 +11,10 @@ void init::init_wifi() {
 
   auto profile = config::Config::GetActiveNetworkProfile();
 
-  ESP_LOGI(TAG, "Initializing WiFi [%s]", profile->name.Get().c_str());
-  ESP_LOGI(TAG, "SSID: %s, password: %s", profile->ssid.Get().c_str(),
-           profile->password.Get().c_str());
-  if (profile->ip_mode.Get() == types::IPMode::STATIC) {
+  ESP_LOGI(TAG, "Initializing WiFi");
+  ESP_LOGI(TAG, "SSID: %s, password: %s", profile->GetSSID().c_str(),
+           profile->GetPassword().c_str());
+  if (profile->GetIPMode() == types::IPMode::STATIC) {
     esp_netif_ip_info_t ip_info{
         .ip = types::ToEspIp4Addr((types::Ipv4)profile->ip),
         .netmask = types::ToEspIp4Addr((types::Ipv4)profile->subnet),
@@ -24,15 +23,15 @@ void init::init_wifi() {
     config::network.SetIP(ip_info);
   }
 
-  if (profile->mode.Get() == types::NetworkMode::AP) {
-    config::network.InitAp(profile->ssid.Get(), profile->password.Get());
+  if (profile->GetMode() == types::NetworkMode::AP) {
+    config::network.InitAp(profile->GetSSID(), profile->GetPassword());
 
     config::network.WaitUntilConnected();
   } else {
     wifi::WifiConnectionProfile prof{
         .auth_mode = WIFI_AUTH_WPA2_PSK,
-        .ssid = profile->ssid.Get(),
-        .password = profile->password.Get(),
+        .ssid = profile->GetSSID(),
+        .password = profile->GetPassword(),
         .user = "",
         .id = "",
     };
