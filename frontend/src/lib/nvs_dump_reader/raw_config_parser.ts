ba0d464f2ea310e7105e9a5c13b33c62b89daf2d
syoch <syoch64@gmail.com>
2023-09-02 10:49:37 +0900

    Update: Added missing member declaretion

--- frontend/src/lib/nvs_dump_reader/raw_config_parser.ts
+++ frontend/src/lib/nvs_dump_reader/raw_config_parser.ts
@@ -1,4 +1,6 @@
 export class RawConfigParser {
+  data: number[];
+
   constructor(data: number[]) {
     if (!data) {
       throw new Error("Data is null");
@@ -12,25 +14,25 @@ export class RawConfigParser {
     return value >>> 0;
   }
 
-  ReadUint16():number {
+  ReadUint16(): number {
     const value = (this.data[0] << 8) | (this.data[1]);
     this.data = this.data.slice(2);
     return value >>> 0;
   }
 
-  ReadUint32():number {
+  ReadUint32(): number {
     const value = (this.data[0] << 24) | (this.data[1] << 16) | (this.data[2] << 8) | this.data[3];
     this.data = this.data.slice(4);
     return value >>> 0;
   }
 
-  ReadUint64():number {
+  ReadUint64(): number {
     const value = (this.data[0] << 56) | (this.data[1] << 48) | (this.data[2] << 40) | (this.data[3] << 32) | (this.data[4] << 24) | (this.data[5] << 16) | (this.data[6] << 8) | this.data[7];
     this.data = this.data.slice(8);
     return value >>> 0;
   }
 
-  ReadData(len: number):number[] {
+  ReadData(len: number): number[] {
     const value = this.data.slice(0, len);
     this.data = this.data.slice(len);
     return value;
