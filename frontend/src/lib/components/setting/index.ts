c9a880f0bdcaaa678e1ea9780fdd745c7f482c9d
syoch <syoch64@gmail.com>
2023-09-03 02:20:35 +0900

    Update: Added some default export members

--- frontend/src/lib/components/setting/index.ts
+++ frontend/src/lib/components/setting/index.ts
@@ -1,4 +1,6 @@
 export { getNs } from "./table";
 export { default as Setting } from "./setting.svelte";
 export { default as EntryNum } from "./entry_num.svelte";
+export { default as EntryText } from "./entry_text.svelte";
+export { default as EntrySelect } from "./entry_select.svelte";
 export { default as AsTab } from "./as_tab.svelte";
