0581ba13a0ce02e00e359f02a6658bbb717375d5
syoch <syoch64@gmail.com>
2023-09-03 02:21:21 +0900

    Update: Fixed IP byteOrder

--- main/src/config/types/ipv4.hpp
+++ main/src/config/types/ipv4.hpp
@@ -9,7 +9,8 @@ using Ipv4 = uint32_t;
 
 inline esp_ip4_addr ToEspIp4Addr(Ipv4 ip) {
   esp_ip4_addr addr;
-  addr.addr = ip;
+  addr.addr = ((ip & 0xFF000000) >> 24) | ((ip & 0x00FF0000) >> 8) |
+              ((ip & 0x0000FF00) << 8) | ((ip & 0x000000FF) << 24);
   return addr;
 }
 
