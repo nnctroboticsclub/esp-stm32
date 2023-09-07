43ab0e6c75b31b6edba7355e1b5be3ea1033621e
syoch <syoch64@gmail.com>
2023-09-07 18:13:38 +0900

    [data_proxy] initial

---
+++ components/data_proxy/include/data_proxy.hpp
@@ -0,0 +1,196 @@
+#pragma once
+
+#include <esp_log.h>
+
+#include <vector>
+#include <cinttypes>
+#include <memory>
+#include <cstring>
+#include <map>
+#include <utility>
+
+#include <connection/data_link/base.hpp>
+#include <connection/data_link/spi.hpp>
+#include <connection/data_link/uart.hpp>
+#include <connection/data_link/i2c_dev.hpp>
+#include <i2c_cxx.hpp>
+
+namespace data_proxy {
+
+enum class BusType { SPI, I2C, UART, Pipe };
+
+using PortID = uint8_t;
+using BusID = uint8_t;
+
+inline BusType GetBusType(BusID bus_id) {
+  return static_cast<BusType>(bus_id >> 6);
+}
+
+enum class CommandKind {
+  kSession = 0,
+  kIO,
+  kGeneral,
+  kReserved1,
+  kReserved2,
+  kReserved3,
+  kReserved4,
+  kReserved5,
+};
+enum class Command : uint8_t {
+  kBusClose = 0x00,
+  kBusNewSPI = 0x01,
+  kBusNewI2C = 0x02,
+  kBusNewNotify = 0x08,
+  kBusDelNotify = 0x09,
+
+  kPortClose = 0x10,
+  kPortNew = 0x11,
+  kPortNewNotify = 0x18,
+  kPortDelNotify = 0x19,
+  kPortErrNotify = 0x1a,
+
+  kIoPortWrite = 0x30,
+  kIoPortRead = 0x31,
+  kIoPortAvail = 0x32,
+  kIoPortDataResponse = 0x38,
+  kIoPortLengthResponse = 0x39,
+
+  kGeneralRequestBusList = 0x40,
+  kGeneralVersion = 0x47,
+  kGeneralBusList = 0x48,
+  kGeneralUnrecoverableError = 0x49,
+  kGeneralRecoverableError = 0x4a,
+  kGeneralRequestPortList = 0x50,
+  kGeneralPortList = 0x58,
+};
+
+struct Packet {
+  union {
+    Command command;
+    uint8_t raw_command;
+  };
+  uint8_t bus_id;
+  uint8_t port_id;
+  uint8_t optional;
+
+  uint32_t data_size;
+  const uint8_t *data;
+
+  ~Packet() {}
+
+  void Debug() const {
+    ESP_LOGI("Packet", "Structure");
+    ESP_LOGI("Packet", "  Command: %d", raw_command);
+    ESP_LOGI("Packet", "  Bus ID: %d", bus_id);
+    ESP_LOGI("Packet", "  Port ID: %d", port_id);
+    ESP_LOGI("Packet", "  Optional: %d", optional);
+    for (size_t i = 0; i < data_size; i++) {
+      ESP_LOGI("Packet", "  Data[%d]: %d", i, data[i]);
+    }
+  }
+};
+
+enum class Errno : uint8_t {
+  kUnknownCommand,
+  kUnknownBus,
+  kUnknownPort,
+  kUnknownError,
+  kMalformedPacket,
+  kMalformedData,
+  kInvalidBusID,
+  kInvalidPortID,
+};
+
+class Link {
+ public:
+  static constexpr uint8_t kPacketHeaderSize =
+      sizeof(Packet) - sizeof(uint32_t);
+
+ private:
+  using T = std::shared_ptr<connection::data_link::RecvAndSend>;
+  T link;
+
+ public:
+  explicit Link(T link);
+
+  void SendPacket(const Packet &packet) const;
+
+  Packet RecvPacket() const;
+};
+
+class Port : public connection::data_link::RecvAndSend {
+  PortID id;
+
+ public:
+  inline explicit Port(PortID id) : id(id) {}
+
+  inline PortID GetID() const { return id; }
+};
+
+template <typename DataLink>
+class HWPort : public Port {
+  DataLink device;
+
+  size_t Recv(std::vector<uint8_t> &buf,
+              TickType_t timeout = 1000 / portTICK_PERIOD_MS) override {
+    return device.Recv(buf, timeout);
+  }
+
+  size_t Send(const std::vector<uint8_t> &buf) override {
+    return device.Send(buf);
+  }
+
+ public:
+  template <typename... Args>
+  inline explicit HWPort(PortID id, Args &&...args)
+      : Port(id), device(std::forward<Args>(args)...) {}
+};
+
+using SPIPort = HWPort<connection::data_link::SPIDevice>;
+using I2CPort = HWPort<connection::data_link::I2CDevice>;
+
+class Bus {
+  std::map<PortID, std::shared_ptr<Port>> ports;
+  virtual std::shared_ptr<Port> CreatePortImpl(uint8_t port_id) = 0;
+
+ public:
+  virtual ~Bus() = default;
+  std::shared_ptr<Port> GetPort(PortID port_id);
+};
+
+class SPIBus : public Bus {
+  idf::SPIMaster master;
+
+ public:
+  ~SPIBus() override = default;
+  inline explicit SPIBus(idf::SPINum bus_id, idf::MOSI const mosi,
+                         idf::MISO const miso, idf::SCLK const sclk)
+      : master(bus_id, mosi, miso, sclk) {}
+  std::shared_ptr<Port> CreatePortImpl(uint8_t port_id) override;
+};
+
+class I2CBus : public Bus {
+  idf::I2CMaster master;
+
+ public:
+  ~I2CBus() override = default;
+  inline explicit I2CBus(idf::I2CNumber bus_id, idf::SDA_GPIO const sda,
+                         idf::SCL_GPIO const scl)
+      : master(bus_id, scl, sda, idf::Frequency::KHz(400)) {}
+
+  std::shared_ptr<Port> CreatePortImpl(uint8_t port_id) override;
+};
+
+class Proxy {
+  class Impl;
+  std::shared_ptr<Impl> impl;
+
+ public:
+  explicit Proxy(Link link);
+
+  std::shared_ptr<Bus> GetBus(BusID bus_id);
+
+  std::thread Start();
+};
+
+}  // namespace data_proxy
---
+++ components/data_proxy/src/data_proxy.cpp
@@ -0,0 +1,317 @@
+#include <data_proxy.hpp>
+
+namespace data_proxy {
+
+namespace {
+template <typename ToCheck, std::size_t ExpectedSize,
+          std::size_t RealSize = sizeof(ToCheck)>
+void check_size() {
+  static_assert(ExpectedSize == RealSize, "Size is off!");
+}
+}  // namespace
+
+Link::Link(T link) : link(link) {
+  check_size<Command, 1>();
+  link->SetTraceEnabled(true);  // TODO(syoch): remove
+}
+
+void Link::SendPacket(const Packet& packet) const {
+  const Packet* packet_ptr = &packet;
+
+  // Send header
+  {
+    const std::vector<uint8_t> buffer(
+        reinterpret_cast<const uint8_t*>(packet_ptr),
+        reinterpret_cast<const uint8_t*>(packet_ptr) + kPacketHeaderSize);
+    this->link->Send(buffer);
+  }
+
+  // Send data
+  if (packet_ptr->data_size) {
+    std::vector<uint8_t> buffer(packet_ptr->data,
+                                packet_ptr->data + packet_ptr->data_size);
+    this->link->SendU32(packet_ptr->data_size);
+    this->link->Send(buffer);
+  }
+}
+
+Packet Link::RecvPacket() const {
+  // ACK
+  ESP_LOGI("Link", "Wait ACK");
+  do {
+    this->link->SendChar(0xa5);
+
+  } while (this->link->RecvChar() != 0xa5);
+  ESP_LOGI("Link", "Got ACK");
+
+  Packet packet{.raw_command = 0,
+                .bus_id = 0,
+                .port_id = 0,
+                .optional = 0,
+                .data_size = 0,
+                .data = nullptr};
+
+  // Recv header
+  {
+    std::vector<uint8_t> buffer(kPacketHeaderSize - sizeof(uint32_t));
+    this->link->Recv(buffer);
+
+    packet.raw_command = buffer[0];
+    packet.bus_id = buffer[1];
+    packet.port_id = buffer[2];
+    packet.optional = buffer[3];
+  }
+
+  // Recv data
+  packet.data_size = this->link->RecvU32();
+  ESP_LOGI("Link", "Data size: %ld", packet.data_size);
+  if (packet.data_size) {
+    std::vector<uint8_t> buffer(packet.data_size, 0);
+    this->link->Recv(buffer);
+
+    auto raw_buf = new uint8_t[packet.data_size];
+    memcpy(raw_buf, buffer.data(), packet.data_size);
+
+    packet.data = raw_buf;
+  }
+
+  return packet;
+}
+
+//* Bus
+std::shared_ptr<Port> Bus::GetPort(PortID port_id) {
+  auto it = ports.find(port_id);
+  if (it == ports.end()) {
+    auto port = CreatePortImpl(port_id);
+    ports[port_id] = port;
+    return port;
+  }
+  return it->second;
+}
+
+//* SPIBus
+std::shared_ptr<Port> SPIBus::CreatePortImpl(uint8_t port_id) {
+  auto port =
+      std::make_shared<SPIPort>(port_id, this->master, (idf::CS)port_id);
+  return port;
+}
+
+//* I2CBus
+std::shared_ptr<Port> I2CBus::CreatePortImpl(uint8_t port_id) {
+  auto port = std::make_shared<I2CPort>(port_id, this->master,
+                                        idf::I2CAddress(port_id));
+  return port;
+}
+
+//* Proxy
+class Proxy::Impl {
+  class HandlerError : public std::runtime_error {
+   public:
+    using std::runtime_error::runtime_error;
+  };
+
+  class InvalidBusID : public HandlerError {
+   public:
+    InvalidBusID() : HandlerError("Invalid bus ID") {}
+  };
+
+  class UnknownBus : public HandlerError {
+   public:
+    UnknownBus() : HandlerError("Unknown bus") {}
+  };
+
+  class UnknownPort : public HandlerError {
+   public:
+    UnknownPort() : HandlerError("Unknown port") {}
+  };
+
+  static constexpr const char* TAG = "Proxy[Impl]";
+  static void (Impl::*handlers[256])(Packet const&);
+
+  Link link;
+  std::map<BusID, std::shared_ptr<Bus>> buses;
+
+  void NotifyNewBus(uint8_t bus_id) {
+    ESP_LOGI(TAG, "Notifying new bus %d", bus_id);
+    Packet response;
+    response.command = Command::kBusNewNotify;
+    response.bus_id = bus_id;
+    response.port_id = 0;
+    response.optional = 0;
+    response.data_size = 0;
+    response.data = nullptr;
+    this->link.SendPacket(response);
+  }
+
+  void NotifyIOPortLength(uint32_t length) {
+    uint8_t buffer[sizeof(uint32_t)];
+    buffer[0] = (length >> 24) & 0xff;
+    buffer[1] = (length >> 16) & 0xff;
+    buffer[2] = (length >> 8) & 0xff;
+    buffer[3] = (length >> 0) & 0xff;
+
+    Packet response;
+    response.command = Command::kIoPortLengthResponse;
+    response.bus_id = 0;
+    response.port_id = 0;
+    response.optional = 0;
+    response.data_size = sizeof(uint32_t);
+    response.data = buffer;
+    this->link.SendPacket(response);
+  }
+
+  void NotifyIOPortData(std::vector<uint8_t> const& data) {
+    Packet response;
+    response.command = Command::kIoPortDataResponse;
+    response.bus_id = 0;
+    response.port_id = 0;
+    response.optional = 0;
+
+    response.data_size = data.size();
+    response.data = data.data();
+    this->link.SendPacket(response);
+  }
+
+  void HandleNotImplemented(Packet const& packet) {
+    ESP_LOGI(TAG, "Not implemented command: %d", packet.raw_command);
+    Packet response;
+    response.command = Command::kGeneralRecoverableError;
+    response.bus_id = packet.bus_id;
+    response.port_id = packet.port_id;
+    response.optional = packet.optional;
+    response.data_size = 0;
+    response.data = nullptr;
+    this->link.SendPacket(response);
+  }
+
+  void HandleNewSPIBus(Packet const& packet) {
+    using enum CommandKind;
+
+    auto& data = packet.data;
+
+    auto bus_id = packet.bus_id;
+
+    ESP_LOGI(TAG, "Creating SPI bus %d", bus_id);
+    ESP_LOGI(TAG, "  - MOSI: %d", data[0]);
+    ESP_LOGI(TAG, "  - MISO: %d", data[1]);
+    ESP_LOGI(TAG, "  - SCLK: %d", data[2]);
+
+    auto bus = std::make_shared<SPIBus>(  //
+        static_cast<idf::SPINum>(bus_id), static_cast<idf::MOSI const>(data[0]),
+        static_cast<idf::MISO const>(data[1]),
+        static_cast<idf::SCLK const>(data[2]));
+    buses[bus_id] = bus;
+
+    this->NotifyNewBus(bus_id);
+  }
+
+  void HandlePortWrite(Packet const& packet) {
+    if (this->buses.contains(packet.bus_id)) {
+      throw UnknownBus();
+    }
+    auto bus = this->buses[packet.bus_id];
+
+    auto port = bus->GetPort(packet.port_id);
+    if (!port) {
+      throw UnknownPort();
+    }
+
+    std::vector<uint8_t> data(packet.data, packet.data + packet.data_size);
+    port->Send(data);
+
+    this->NotifyIOPortLength(data.size());
+  }
+
+  void HandlePortRead(Packet const& packet) {
+    if (this->buses.contains(packet.bus_id)) {
+      throw UnknownBus();
+    }
+    auto bus = this->buses[packet.bus_id];
+
+    auto port = bus->GetPort(packet.port_id);
+    if (!port) {
+      throw UnknownPort();
+    }
+
+    std::vector<uint8_t> data(packet.data_size, 0);
+    port->Recv(data);
+
+    this->NotifyIOPortData(data);
+  }
+
+  void HandleNewI2CBus(Packet const& packet) {
+    using enum CommandKind;
+
+    auto& data = packet.data;
+
+    auto bus_id = packet.bus_id;
+
+    idf::I2CNumber i2c_number = ([bus_id]() {
+      if (bus_id == 0) {
+        return idf::I2CNumber::I2C0();
+      } else if (bus_id == 1) {
+        return idf::I2CNumber::I2C1();
+      } else {
+        throw InvalidBusID();
+      }
+    })();
+
+    ESP_LOGI(TAG, "Creating I2C bus %d", bus_id);
+    ESP_LOGI(TAG, "  - SDA: %d", data[0]);
+    ESP_LOGI(TAG, "  - SCL: %d", data[1]);
+
+    auto bus = std::make_shared<I2CBus>(  //
+        i2c_number, static_cast<idf::SDA_GPIO const>(data[0]),
+        static_cast<idf::SCL_GPIO const>(data[1]));
+    buses[bus_id] = bus;
+
+    this->NotifyNewBus(bus_id);
+  }
+
+ public:
+  Impl(Link link) : link(link) {
+    using enum Command;
+
+    for (auto& handler : handlers) {
+      handler = &Impl::HandleNotImplemented;
+    }
+    handlers[(uint8_t)kBusNewSPI] = &Impl::HandleNewSPIBus;
+    handlers[(uint8_t)kBusNewI2C] = &Impl::HandleNewI2CBus;
+    handlers[(uint8_t)kIoPortWrite] = &Impl::HandlePortWrite;
+    handlers[(uint8_t)kIoPortRead] = &Impl::HandlePortRead;
+  }
+
+  [[noreturn]] void Thread() {
+    while (true) {
+      ESP_LOGI(TAG, "Waiting for packet");
+      auto packet = this->link.RecvPacket();
+      auto handler = handlers[packet.raw_command];
+      try {
+        (this->*handler)(packet);
+      } catch (InvalidBusID&) {
+        uint8_t buf[1] = {static_cast<uint8_t>(Errno::kInvalidBusID)};
+
+        Packet response;
+        response.command = Command::kGeneralRecoverableError;
+        response.bus_id = packet.bus_id;
+        response.port_id = packet.port_id;
+        response.optional = packet.optional;
+        response.data_size = 1;
+        response.data = buf;
+        this->link.SendPacket(response);
+      }
+
+      delete[] packet.data;
+    }
+  }
+};
+
+void (Proxy::Impl::*Proxy::Impl::handlers[256])(Packet const&);
+
+Proxy::Proxy(Link link) : impl(std::make_unique<Impl>(link)) {}
+
+std::thread Proxy::Start() {
+  std::thread t([this]() { this->impl->Thread(); });
+  return t;
+}
+}  // namespace data_proxy
